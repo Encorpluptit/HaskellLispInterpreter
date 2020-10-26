e49f1ea8bf89d16c42f1eaa9614553632ec7232d
BERNARD Damien <damien.bernard@epitech.eu>
2020-10-26 15:16:02 +0100

    add(lib): Adding IOUtils module in lib.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
 import Lib
+import Parsing
+import IOUtils
+
+--main :: IO ()
+--main = do
+--    someFunc
+--    case runParser parseInt "-42a" of
+--        Left a  -> putStrLn a
+--        Right b -> print b
 
 main :: IO ()
-main = someFunc
+main = exitError "DIE !"
